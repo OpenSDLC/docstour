cff113a87e2f833df6d3cf84f8db8959507d32da
MxJRd <51065805+MxJRd@users.noreply.github.com>
2021-03-29 14:37:20 -0700

    First commit, typescript enabled fullstack application

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,9 +1,9 @@
-import * as express from 'express'
+import express, { Request, Response } from 'express';
 const os = require('os');
 
 const app = express();
 
 app.use(express.static('dist'));
-app.get('/api/getUsername', (req: express.Request, res: express.Response) => res.send({ username: os.userInfo().username }));
+app.get('/api/getUsername', (req: Request, res: Response) => res.send({ username: os.userInfo().username }));
 
 app.listen(process.env.PORT || 3000, () => console.log(`Listening on port ${process.env.PORT || 3000}!`));
