092742ba6e8fbfe0b14b3c67b9593002cf1e990d
MxJRd <51065805+MxJRd@users.noreply.github.com>
2021-04-01 16:52:46 -0700

    Redux store working with asynchronous database calls
    Co-authored-by: Tolan Thornton <31934392+taoantaoan@users.noreply.github.com>
    Co-authored-by: Anthony Torrero <66744064+Anthonytorrero@users.noreply.github.com>
    Co-authored-by: Tammy Le <62489463+letammy979@users.noreply.github.com>

--- src/client/components/LandingPage/Doc.tsx
+++ src/client/components/LandingPage/Doc.tsx
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import { Card, CardActions, CardContent, Button, Link } from '@material-ui/core';
+import { RootState } from '../../redux/rootReducer';
+import { DocumentState, Document } from '../../redux/documentReducer';
+import { connect } from 'react-redux';
 
 interface Props {
-  name: string,
-  url: string,
-  notes: string
+  docList: Document[];
 }
 
 
@@ -15,21 +16,33 @@ class DocContainer extends Component<Props> {
 
 
 
-render() {
-  return (
-    <Card className="card">
-      <CardContent>
-        Doc of the Day <br></br>
-        <Link>{this.props.url}</Link>
-      </CardContent>
+  render() {
+    const docList = this.props.docList.map(document => {
+      return <li>Name: {document.name} | URL: {document.url} | Notes: {document.notes}</li>
+      // return <li>document.name</li>
+    })
 
-      <CardActions>
-        <Button size="small" variant="contained">Edit</Button>
-        <Button size="small" variant="contained" color="secondary">Delete</Button>
-      </CardActions>
-    </Card>
-  )
-}
+    return (
+      <Card className="card">
+        <CardContent>
+          Name <br></br>
+          <Link>{}</Link>
+          Notes
+        </CardContent>
+
+        <CardActions>
+          <Button size="small" variant="contained">Edit</Button>
+          <Button size="small" variant="contained" color="secondary">Delete</Button>
+        </CardActions>
+      </Card>
+    )
+  }
 }
 
-export default DocContainer;
+const mapStateToProps = (state: RootState) => {
+  return {
+    docList: state.document.docList
+  }
+};
+
+export default connect(mapStateToProps)(DocContainer);
--- src/client/redux/actionTypes.ts
+++ src/client/redux/actionTypes.ts
@@ -3,7 +3,7 @@
 export const ADD_DOC = 'ADD_DOC';
 export const EDIT_DOC ='EDIT_DOC';
 export const DELETE_DOC = 'DELETE_DOC';
-
+export const FETCH_DOCS = 'FETCH_DOCS';
 export const INCREMENT = 'INCREMENT';
 export const DECREMENT = 'DECREMENT';
 
